9f34d92d39ebeacaaf755113e8379e1ac9526acd
Alexander Shepetko <a@shepetko.com>
2017-12-02 10:35:16 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __license__ = 'MIT'
 
 
 def _init():
-    from pytsite import lang, router
+    from pytsite import lang, router, reg
     from plugins import permissions, settings
 
     lang.register_package(__name__)
@@ -16,8 +16,8 @@ def _init():
     permissions.define_permission('mail_settings@manage_settings', 'mail_settings@manage_mail_settings', 'app')
     settings.define('mail', _settings_form.Form, 'mail_settings@mail', 'fa fa-envelope', 'mail_settings@manage_settings')
 
-    if not settings.get('mail.from'):
-        settings.put('mail.from', 'info@' + router.server_name())
+    if not reg.get('mail.from'):
+        reg.put('mail.from', 'info@' + router.server_name())
 
 
 _init()
