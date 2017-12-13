af512059f818db7aa94ff18726523a4b1fb5bf37
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:49:20 +0200

    Release 1.2

--- __init__.py
+++ __init__.py
@@ -1,23 +1,21 @@
 """PytSite Mail Settings Plugin
 """
-from . import _settings_form
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def _init():
+def plugin_load():
     from pytsite import lang, router, reg
     from plugins import permissions, settings
+    from . import _settings_form
 
     lang.register_package(__name__)
 
     permissions.define_permission('mail_settings@manage_settings', 'mail_settings@manage_mail_settings', 'app')
-    settings.define('mail', _settings_form.Form, 'mail_settings@mail', 'fa fa-envelope', 'mail_settings@manage_settings')
+    settings.define('mail', _settings_form.Form, 'mail_settings@mail', 'fa fa-envelope',
+                    'mail_settings@manage_settings')
 
     if not reg.get('mail.from'):
         reg.put('mail.from', 'info@' + router.server_name())
-
-
-_init()
