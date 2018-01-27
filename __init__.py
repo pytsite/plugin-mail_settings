7711f0d8a6bb7c3d5c83f0b7e386fb771be2e0e8
Alexander Shepetko <a@shepetko.com>
2018-01-27 05:12:29 +0200

    Release 1.3

--- __init__.py
+++ __init__.py
@@ -6,16 +6,14 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def plugin_load():
+def plugin_load_uwsgi():
     from pytsite import lang, router, reg
-    from plugins import permissions, settings
+    from plugins import settings
     from . import _settings_form
 
     lang.register_package(__name__)
 
-    permissions.define_permission('mail_settings@manage_settings', 'mail_settings@manage_mail_settings', 'app')
-    settings.define('mail', _settings_form.Form, 'mail_settings@mail', 'fa fa-envelope',
-                    'mail_settings@manage_settings')
+    settings.define('mail', _settings_form.Form, 'mail_settings@mail', 'fa fa-envelope', 'dev')
 
     if not reg.get('mail.from'):
         reg.put('mail.from', 'info@' + router.server_name())
